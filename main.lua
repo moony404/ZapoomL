492f34de11e0ad71ce69357d0318a7b5dc01801b
moony404 <theo.marliac63@gmail.com>
2023-09-11 19:14:35 +0200

    added the x movement

--- main.lua
+++ main.lua
@@ -4,14 +4,17 @@ circle.y = 0
 player = {}
 player.x = 0
 player.y = 0
-player.speed = 10
+player.speed = 100
 
 function love.draw()
    love.graphics.rectangle("fill", player.x, player.y, 100, 100)
 end
 
 function love.update(dt)
-   if love.keyboard.isDown("left") then
+   if love.keyboard.isDown("right") then
       player.x = player.x + player.speed * dt
+   end
+   if love.keyboard.isDown("left") then
+      player.x = player.x - player.speed * dt
    end
 end
