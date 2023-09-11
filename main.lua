f967e65d984003cfbe79c7e6002801afdb7901be
moony404 <theo.marliac63@gmail.com>
2023-09-11 19:07:42 +0200

    YESSS ADDED MOVEMENT !!!! LETS GOOOOOO I'M SO HAPPY

--- main.lua
+++ main.lua
@@ -1,8 +1,17 @@
 circle = {}
 circle.x = 0
 circle.y = 0
+player = {}
+player.x = 0
+player.y = 0
+player.speed = 10
 
 function love.draw()
-   love.graphics.circle("fill", 100, 0, 100)
-   love.graphics.print("ZapoomL is launching...", 300, 400)
+   love.graphics.rectangle("fill", player.x, player.y, 100, 100)
+end
+
+function love.update(dt)
+   if love.keyboard.isDown("left") then
+      player.x = player.x + player.speed * dt
+   end
 end
