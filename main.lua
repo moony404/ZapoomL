08553c0c59dca035edcdb5d8701e5a6b1dade648
moony404 <theo.marliac63@gmail.com>
2023-09-13 17:24:46 +0200

    update main.lua with comments

--- main.lua
+++ main.lua
@@ -2,52 +2,51 @@
 -- variables
 function love.load() -- love2d's function to load some code before launch
    -- libraries
-   anim8 = require('libraries/anim8')
+   anim8 = require('libraries/anim8') -- imported anim8.lua from the libraries folder in ZapoomL
    -- variables
-   player = {}
+   player = {} -- player table
+   player.animations = {} -- player.animations table
    player.x = 0 -- the x coordinates of the player
    player.y = 0 -- the y coordinates of the player
    player.speed = 200 -- the speed of the player & this variable is used to calculate the movement of the player
    -- img variables
-   player.sprite = love.graphics.newImage('img/player.png')
-   player.spriteSheet = love.graphics.newImage('img/player-sheet.png')
-   -- slice the player.spriteSheet to animated it later
-   player.grid = anim8.newGrid(12 , 18, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
-   background = love.graphics.newImage('img/background.png')
+   player.sprite = love.graphics.newImage('img/player.png') -- add the player sprite image
+   player.spriteSheet = love.graphics.newImage('img/player-sheet.png') -- add the payer sprite sheet image
+   player.grid = anim8.newGrid(12 , 18, player.spriteSheet:getWidth(), player.spriteSheet:getHeight()) -- slice the player.spriteSheet to animated it later
+   background = love.graphics.newImage('img/background.png') -- add the background image
 end
 -- window settings
 love.window.setTitle("ZapoomL") -- the title of the window
 love.window.setFullscreen(true) -- make the window of ZapoomL in fullscreen mode so it fit on all monitor/screen
 -- the draw function draws the player and the background
-function love.draw()
+function love.draw() -- love2d's function to draw somthing on the screen
    love.graphics.draw(background) -- draw the background
-   love.graphics.draw(player.sprite, player.x, player.y, 0, 1.5) -- draw the player sprite
+   love.graphics.draw(player.sprite, player.x, player.y, 0, 1.5) -- draw the player sprite at 0° rotation and 1.5 scale
 end
 -- love2d's function
-function love.update(dt)
+function love.update(dt) -- love2d's function to run the game smoothly on any device with the dt variable, the function is like a infinite loop
    -- player movement
    -- right
-   if love.keyboard.isDown("right") then
+   if love.keyboard.isDown("right") then -- check if the right arrow key is down
       player.x = player.x + player.speed * dt
    end
    -- left
-   if love.keyboard.isDown("left") then
+   if love.keyboard.isDown("left") then -- check if the left arrow key is down
       player.x = player.x - player.speed * dt
    end
    -- down
-   if love.keyboard.isDown("down") then
+   if love.keyboard.isDown("down") then -- check if the down arrow key is down
       player.y = player.y + player.speed * dt
    end
    -- up
-   if love.keyboard.isDown("up") then
+   if love.keyboard.isDown("up") then -- check if the up arrow key is down
       player.y = player.y - player.speed * dt
    end
    -- shortcut for keyboard
-   if love.keyboard.isDown("backspace") then -- backspace key is to close the window
-      love.window.close() -- to close the window
+   if love.keyboard.isDown("backspace") then -- check if the backspace key is down
+      love.window.close() -- close the window
    end
-   if love.keyboard.isDown("lctrl") then -- left control key is to minimize the window
-      love.window.minimize() -- to minimize the window
+   if love.keyboard.isDown("lctrl") then -- check if the left control key is down
+      love.window.minimize() -- minimize the window
    end
-   -- close the app keybind
 end
